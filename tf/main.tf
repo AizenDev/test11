6e08ae049fb4ebd3e844f2c1c5fdeb7d3e3a1a18
root <root@main-vm.europe-north1-c.c.xenon-pier-459606-n2.internal>
2025-07-15 08:58:31 +0000

    Initial commit

---
+++ tf/main.tf
@@ -0,0 +1,122 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  credentials = file(var.credentials_file)
+  zone        = var.zone
+}
+
+resource "google_compute_disk" "my_disk_1" {
+  name = "my-disk-1"
+  size = var.disk_size_gb
+  type = var.disk_type
+  zone = var.zone
+}
+
+resource "google_compute_disk" "my_disk_2" {
+  name = "my-disk-2"
+  size = var.disk_size_gb
+  type = var.disk_type
+  zone = var.zone
+}
+
+resource "google_compute_instance" "vm_instance_1" {
+  name         = var.vm_name_1
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    source = google_compute_disk.my_disk_1.name
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+  
+  metadata = {
+    ssh-keys = "jenkins-agent:${file("~/.ssh/gcp_key.pub")}"
+  }
+
+  tags = [var.vm_tags]
+
+  service_account {
+    email  = var.service_account_email
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+}
+
+
+resource "google_compute_instance" "vm_instance_2" {
+  name         = var.vm_name_2
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    source = google_compute_disk.my_disk_2.name
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  tags = [var.vm_tags]
+
+  metadata = {
+    ssh-keys = "jenkins-agent:${file("~/.ssh/gcp_key.pub")}"
+  }
+
+  service_account {
+    email  = var.service_account_email
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+}
+
+resource "google_compute_firewall" "allow_http_https" {
+   name        = "allow-http-https"
+   network     = "default"
+   description = "Allow HTTP and HTTPS from anywhere"
+   allow {
+     protocol = "tcp"
+     ports    = ["80", "443"]
+   }
+
+   source_ranges = ["0.0.0.0/0"]
+   target_tags = ["web-server"]
+ }
+
+resource "google_compute_firewall" "ssh" {
+  name    = "allow-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["allow-ssh"]
+}
+
+output "instance_name" {
+  value = google_compute_instance.vm_instance_1.name
+}
+
+output "instance_ip_1" {
+  description = "External IP address of the instance"
+  value       = google_compute_instance.vm_instance_1.network_interface[0].access_config[0].nat_ip
+}
+
+output "instance_ip_2" {
+  description = "External IP address of the instance"
+  value       = google_compute_instance.vm_instance_2.network_interface[0].access_config[0].nat_ip
+}
---
+++ tf/variables.tf
@@ -0,0 +1,70 @@
+variable "project_id" {
+  type        = string
+  description = "xenon-pier-459606-n2"
+}
+
+variable "region" {
+  type        = string
+  description = "Регион Google Cloud"
+  default     = "europe-north1"
+}
+
+variable "zone" {
+  type        = string
+  description = "Зона Google Cloud"
+  default     = "europe-north1-c"
+}
+
+variable "vm_name_1" {
+  type        = string
+  description = "Имя виртуальной машины"
+  default     = "terraform-vm-1"
+}
+
+variable "vm_name_2" {
+  type        = string
+  description = "Имя виртуальной машины"
+  default     = "terraform-vm-2"
+}
+
+variable "machine_type" {
+  type        = string
+  description = "Тип машины (например, e2-medium)"
+  default     = "e2-small"
+}
+
+variable "image" {
+  type        = string
+  description = "Образ операционной системы"
+  default     = "ubuntu-minimal-2204-jammy-v20250530"
+}
+
+variable "disk_size_gb" {
+  type        = number
+  description = "Размер диска в гигабайтах"
+  default     = 40
+}
+
+variable "disk_type" {
+  type        = string
+  description = "Тип диска (например, pd-standard)"
+  default     = "pd-balanced"
+}
+
+variable "vm_tags" {
+  type        = string
+  description = "Теги для виртуальной машины"
+  default     = ["allow-ssh", "web-server"]
+}
+
+variable "credentials_file" {
+  type        = string
+  description = "Path to service account key file"
+  default     = "creds.json"
+}
+
+variable "service_account_email" {
+  type        = string
+  description = "Сервисный аккаунт"
+  default     = "543116208909-compute@developer.gserviceaccount.com"
+}
